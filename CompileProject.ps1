a1e1bad828c9e959131989c0cc87da410515eef8
Jonas Reich <mail@jonasreich.de>
2022-03-23 19:49:29 +0100

    Fixed automation test and commandlet invocations. Changed automation tools ps1 names to be agnostic of UE major version (UE instead of UE4/UE5)

--- CompileProject.ps1
+++ CompileProject.ps1
@@ -1,17 +1,34 @@
 using namespace System.Management.Automation
 
 param(
+    [String]
+    $RootFolder = "",
+
     [Boolean]
     $UseBuildGraph = $true,
 
-    [String]
-    $RootFolder = ""
+    [Switch]
+    $RunTests,
+
+    [Switch]
+    $UpdateProjectVersion,
+
+    [Switch]
+    $CompileBlueprints,
+
+    [Switch]
+    $FixupRedirectors
 )
 
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
+# Return all verbose output, so we don't have to specify it on individual calls
+# -> all internal commands from OpenUnrealAutomationTools.psm1 can output verbose logs that actually show up in console.
+$global:VerbosePreference = 'continue'
+# set to a different color than Yellow so it stands out less and warnings are better recognizable
+$Host.PrivateData.VerboseForegroundColor = 'Cyan'
 
-$HasCore7Features = $PSVersionTable.PSEdition -eq "Desktop" -or $PSVersionTable.PSVersion -lt "7.0.0"
+$HasCore7Features = -not ($PSVersionTable.PSEdition -eq "Desktop" -or $PSVersionTable.PSVersion -lt "7.0.0")
 if ($HasCore7Features) {
     Write-Warning "This script requires Powershell Core 7 or later for all functionality.
     You are likely still using an old Desktop version that is shipped with Windows (e.g. Windows PowerShell 5.1).
@@ -19,8 +36,10 @@ if ($HasCore7Features) {
 }
 
 $ScriptDirectory = Split-Path $MyInvocation.MyCommand.Path -Parent
+$TimeStamp = Get-Date -Format "yyyy-MM-dd-HH-mm"
+
 $UProjectPath = ""
-Import-Module -Name "$ScriptDirectory/Ue4BuildTools.psm1" -Verbose -Force
+Import-Module -Name "$ScriptDirectory/OpenUnrealAutomationTools.psm1" -Verbose -Force
 
 if ([string]::IsNullOrEmpty($RootFolder)) {
     $RootFolder = Resolve-Path "$ScriptDirectory/.."
@@ -35,6 +54,7 @@ if ([string]::IsNullOrEmpty($RootFolder)) {
         $RootFolder = $ParentDirectory.FullName
     }
 } else {
+    $RootFolder = Resolve-Path $RootFolder
     $UProjectPath = Resolve-Path "$RootFolder/*.uproject"
 }
 
@@ -50,7 +70,7 @@ if ($UProjectPath -match "(?<ProjectName>[a-zA-Z0-9]+).uproject") {
     Write-Error "Project name not found in uproject file path '$UProjectPath'"
 }
 
-$UProject = Open-Ue4Project $UProjectPath
+$UProject = Open-UEProject $UProjectPath
 
 Write-Output "-------------------------------------------------"
 Write-Output "Compiling Game Project"
@@ -60,44 +80,68 @@ if ($UseBuildGraph) {
     # Compile editor binaries with BuildGraph
     # Buildgraph parameter names that contain colons (e.g. '-set:ProjectName') must be quoted.
     # Otherwise powershell inserts an unwanted space
-    Start-Ue4 UAT BuildGraph -script="$ScriptDirectory\Graph.xml" -target="Compile Game Editor" "-Set:ProjectName=`"$ProjectName`"" "-Set:ProjectDir=`"$RootFolder`"" "-Set:BuildConfig=Development"
+    Start-UE UAT BuildGraph -script="$ScriptDirectory\Graph.xml" -target="Compile Game Editor" "-Set:ProjectName=`"$ProjectName`"" "-Set:ProjectDir=`"$RootFolder`"" "-Set:BuildConfig=Development"
 } else {
     # Regenerate project files
-    Start-Ue4 UBT -projectfiles -project="$UProjectPath" -game -rocket -progress
+    Start-UE UBT -projectfiles -project="$UProjectPath" -game -rocket -progress
 
     # Compile editor and standalone binaries via UBT
     $EditorTargetName = $ProjectName+"Editor"
-    Start-Ue4 UBT "$EditorTargetName" "Development" "Win64" -project="$UProjectPath" -NoHotReloadFromIDE -editorrecompile -progress -noubtmakefiles -utf8output
-    Start-Ue4 UBT "$ProjectName" "Development" "Win64" -project="$UProjectPath" -NoHotReloadFromIDE -editorrecompile -progress -noubtmakefiles -utf8output
+    Start-UE UBT "$EditorTargetName" "Development" "Win64" -project="$UProjectPath" -NoHotReloadFromIDE -editorrecompile -progress -noubtmakefiles -utf8output
+    Start-UE UBT "$ProjectName" "Development" "Win64" -project="$UProjectPath" -NoHotReloadFromIDE -editorrecompile -progress -noubtmakefiles -utf8output
 }
 
-Write-Output "-------------------------------------------------"
-Write-Output "Run Tests"
-Write-Output "-------------------------------------------------"
-
-# Launch UE4 with tests
-$EditorTests = $true
-$TestArgs = If ($EditorTests) { @("-editor", "-editortest") } else { @("-game", "-gametest") }
-$HeadlessArgs = @("-unattended", "-buildmachine", "-stdout", "-nullrhi", "-nopause", "-nosplash" )
-
-$ExtraTests = "OpenUnrealUtilities" # Fill from cmdline?
-$TestFilter = "$ProjectName+Project.Functional+$ExtraTests"
-
-$RunTestCmdArg = "-ExecCmds=`"Automation RunTests Now $TestFilter;Quit`""
-$TestTimestamp = Get-Date -Format "yyyy-MM-dd-HH-mm"
-$TestReportPath = "$ScriptDirectory/TestReport-$TestTimestamp"
-Write-Output "Executing automation tests..."
-
-$TestMap = "/Engine/Maps/Entry"
-
-Start-Ue4 EditorCmd "$UProjectPath" "$TestMap" $TestArgs $RunTestCmdArg $HeadlessArgs -ReportExportPath="$TestReportPath"
-
-$Version = Get-Ue4ProjectVersion
-
-if ($HasCore7Features) {
-    [SemanticVersion]$SemVer = $Version
-    $SemVer
+if ($RunTests) {
+    Write-Output "-------------------------------------------------"
+    Write-Output "Run Tests"
+    Write-Output "-------------------------------------------------"
+    
+    # Launch Unreal with tests
+    $EditorTests = $true
+    $TestArgs = If ($EditorTests) { "-editor" } else { "-game" }
+    
+    # Fill from cmdline?
+    $ExtraTests = "OpenUnrealUtilities"
+    $TestFilter = "$ProjectName+Project.Functional+$ExtraTests"
+    $TestReportPath = "$ScriptDirectory/Saved/TestReport-$TimeStamp"
+    
+    Start-UETests -TestFilter $TestFilter -ReportExportPath $TestReportPath -Target Game
 }
-#Set-Ue4ProjectVersion $SemVer.
 
+if ($UpdateProjectVersion) {
+    Write-Output "-------------------------------------------------"
+    Write-Output "Update Project Version SemVer"
+    Write-Output "-------------------------------------------------"
+    
+    $Version = Get-UEProjectVersion
+    
+    if ($HasCore7Features) {
+        [SemanticVersion]$SemVer = $Version
 
+        # This just force writes the semver back to config atm
+        # Could be extended to actuallz change the semver, replace it, etc.
+        # Powershell 7 Core introduces SemanticVersion class (see above) that allows properly incrementing Minor/Patch components
+        Set-UEProjectVersion $SemVer
+    } else {
+        Write-Output "Skipped because of too low PowerShell version"
+    }
+}
+
+if ($CompileBlueprints) {
+    Write-Output "-------------------------------------------------"
+    Write-Output "Blueprint Compile"
+    Write-Output "-------------------------------------------------"
+    
+    # See UCompileAllBlueprintsCommandlet::InitCommandLine for list of additional command line parameters
+    Start-UECommandlet CompileAllBlueprints
+}
+
+if ($FixupRedirectors) {
+    Write-Output "-------------------------------------------------"
+    Write-Output "Fixup Redirectors"
+    Write-Output "-------------------------------------------------"
+    
+    # Other flags:
+    # -SKIPMAPS, -MAPSONLY -SkipDeveloperFolders -NODEV -OnlyDeveloperFolders
+    Start-UECommandlet ResavePackages -fixupredirects -autocheckout -PROJECTONLY
+}
---
+++ OpenUnrealAutomationTools.psm1
@@ -0,0 +1,292 @@
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
+
+$ScriptDirectory = Split-Path $MyInvocation.MyCommand.Path -Parent
+Import-Module -Name "$ScriptDirectory/IniContent.psm1" -Force -Verbose
+
+$UEEnvironmentVariables = @(
+    "CurrentProjectName",
+    "CurrentProjectPath",
+    "CurrentProjectDirectory",
+    "CurrentUProject",
+    "CurrentEngineAssociation",
+    "EngineInstallRoot"
+)
+
+foreach ($VariableName in $UEEnvironmentVariables) {
+    Set-Variable -Name $VariableName -Value $null
+}
+
+function Assert-UEEnvironment {
+    foreach ($VariableName in $UEEnvironmentVariables) {
+        if ($null -eq (Get-Variable $VariableName).Value) {
+            Write-Error "UE environment variable '$VariableName' not set!`nPlease load a project via Open-UEProject!"
+        }
+    }
+}
+
+$DefaultEmptyMap = "`"/OpenUnrealUtilities/Runtime/EmptyWorld`""
+$HeadlessArgs = @("-unattended", "-buildmachine", "-stdout", "-nopause", "-nosplash")
+
+<#
+.SYNOPSIS
+Opens a UE project and sets the current project and engine path.
+.DESCRIPTION
+Opens a UE project file, loads the json structure and saves it in a hidden global variable.
+This also resolves and caches the engine install root of the project, which is the foundation for many other
+functions in the UEBuildTools module that expect the EngineInstallRoot cache to be set.
+
+The function returns a copy of the UProject as object tree so you can retreive metadata from it.
+#>
+function Open-UEProject {
+    param(
+        [String]
+        $ProjectPath
+    )
+    $script:CurrentProjectPath = Resolve-Path $ProjectPath
+    $script:CurrentProjectName = (Get-Item $CurrentProjectPath).Name -replace ".uproject", ""
+    $script:CurrentProjectDirectory = (Get-Item $CurrentProjectPath).Directory
+
+    $script:CurrentUProject = ConvertFrom-Json -InputObject (Get-Content -raw $ProjectPath)
+    
+    $script:CurrentEngineAssociation = $CurrentUProject.EngineAssociation
+    $CustomBuildEnginePath = (Get-ItemProperty "Registry::HKEY_CURRENT_USER\SOFTWARE\Epic Games\Unreal Engine\Builds")."$CurrentEngineAssociation"
+    if (Test-Path $CustomBuildEnginePath) {
+        $script:EngineInstallRoot = Resolve-Path $CustomBuildEnginePath
+    }
+    else {
+        $InstalledEnginePath = (Get-ItemProperty "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\EpicGames\Unreal Engine\$CurrentEngineAssociation" -Name "InstalledDirectory").InstalledDirectory
+        $script:EngineInstallRoot = Resolve-Path ($InstalledEnginePath)
+    }
+
+    Write-Verbose "Set current UE project to $CurrentProjectName ($CurrentProjectPath)"
+    Write-Verbose "Engine association: $CurrentEngineAssociation ($EngineInstallRoot)"
+
+    Assert-UEEnvironment
+    
+    return $CurrentUProject
+}
+
+# Programs available in UE Engine that can be started with Start-UE function.
+enum UEProgram {
+    # UE Automation Tool
+    UAT
+    # UE Build Tool
+    UBT
+    # UE Editor
+    Editor
+    # UE Editor (command line)
+    EditorCmd
+}
+
+$ProgramPaths = @{
+    [UEProgram]::UAT       = "\Engine\Build\BatchFiles\RunUAT.bat"
+    [UEProgram]::UBT       = "\Engine\Build\BatchFiles\Build.bat"
+    [UEProgram]::Editor    = "\Engine\Binaries\Win64\UE4Editor.exe"
+    [UEProgram]::EditorCmd = "\Engine\Binaries\Win64\UE4Editor-Cmd.exe"
+}
+
+<#
+.SYNOPSIS
+    Returns the absolute path to a UE program.
+.DESCRIPTION
+    Takes an input program code and returns the absolute file path to the program executable inside the current active UE engine directory.
+    This function throws an error if you did not previously open a UE project with Open-UEProject.
+#>
+function Get-UEProgramPath {
+    param(
+        [Parameter(Mandatory = $true)]
+        [UEProgram]
+        $Program
+    )
+    Assert-UEEnvironment
+    $RelativePath = $ProgramPaths[$Program]
+    Resolve-Path  "$EngineInstallRoot\$RelativePath"
+}
+
+<#
+.SYNOPSIS
+    Recursively expands arrays and generic object lists by flattening them to a simple object array
+#>
+function Expand-Array {
+    param($Arguments)
+    $Arguments | ForEach-Object {
+        if ($_ -is [array] -or $_ -is [System.Collections.Generic.List[System.Object]]) {
+            Expand-Array $_
+        }
+        else {
+            $_
+        }
+    }
+}
+
+<#
+.SYNOPSIS
+    Starts an UE program with arbitrary command-line arguments.
+.DESCRIPTION
+    Takes an input program code and runs the program executable inside the current active UE engine directory.
+    This function throws an error if you did not previously open a UE project with Open-UEProject.
+#>
+function Start-UE {
+    param(
+        [Parameter(Mandatory = $true)]
+        [UEProgram]
+        $Program,
+
+        # Any remaining arguments that should be passed to the UE program
+        [parameter(ValueFromRemainingArguments = $true)]
+        $RemainingArguments
+    )
+    Assert-UEEnvironment
+    $ProgramPath = Get-UEProgramPath -Program $Program
+    $ExpandedArgs = Expand-Array $RemainingArguments
+    Write-Verbose "Starting UE $Program with the following command-line:`n$ProgramPath $ExpandedArgs"
+    &"$ProgramPath" $ExpandedArgs
+    if (-not $LASTEXITCODE -eq 0) {
+        Write-Error "`"$ProgramPath`" exited with code $LASTEXITCODE"
+    }
+}
+
+# Target configurations for running unit tests. Either in game or editor mode.
+enum UeTestTarget {
+    # Run Game tests
+    Game
+    # Run Editor tests
+    Editor
+}
+
+function Start-UETests {
+    param(
+        [Parameter(Mandatory = $true)]
+        [String]
+        $TestFilter,
+
+        [Parameter(Mandatory = $true)]
+        [String]
+        $ReportExportPath,
+
+        [Parameter(Mandatory = $true)]
+        [UeTestTarget]
+        $Target,
+
+        # Any remaining arguments that should be passed to the UE program
+        [parameter(ValueFromRemainingArguments = $true)]
+        $RemainingArguments
+    )
+    Assert-UEEnvironment
+    $TargetArg = if ($Target -eq [UeTestTarget]::Game) { @("-game", "-gametest") } else { @("-editor", "-editortest") }
+    $RunTestCmdArg = "-ExecCmds=`"Automation RunTests Now $TestFilter;Quit`""
+    $ReportArg = "-ReportExportPath=`"$ReportExportPath`""
+
+    Start-UE EditorCmd $CurrentProjectPath $DefaultEmptyMap $TargetArg $HeadlessArgs "-nullrhi" $RunTestCmdArg $ReportArg $RemainingArguments
+
+    # Delete everything but the json from the test results.
+    $ReportFiles = Get-ChildItem $ReportExportPath
+    foreach ($File in $ReportFiles) {
+        if ($File.Name.EndsWith(".json")) { continue; }
+        Remove-Item $File.FullName
+    }
+    
+    # Copy over our customized test report viewer template
+    Copy-Item -Path "$ScriptDirectory\TestReportViewer_Template\*" -Destination "$ReportExportPath\" -Recurse
+}
+
+
+<#
+.SYNOPSIS
+    Starts a UE commandlet with arbitrary command-line arguments.
+.DESCRIPTION
+    This function throws an error if you did not previously open a UE project with Open-UEProject.
+#>
+
+function Start-UECommandlet {
+    param(
+        [Parameter(Mandatory = $true)]
+        [String]
+        $CommandletName,
+
+        [String]
+        $TargetMap = $DefaultEmptyMap,
+
+        [switch]
+        $AllowCommandletRendering,
+
+        # Any remaining arguments that should be passed to the UE program
+        [parameter(ValueFromRemainingArguments = $true)]
+        $RemainingArguments
+    )
+    Assert-UEEnvironment
+    $RHIArg = if ($AllowCommandletRendering) { "-AllowCommandletRendering" } else { "-nullrhi" }
+    Start-UE EditorCmd $CurrentProjectPath $TargetMap $HeadlessArgs $RHIArg "-run=$CommandletName" $RemainingArguments
+}
+
+function Get-UEConfigPath {
+    param(
+        [parameter(Mandatory = $true)] [string] $ConfigName,
+        [bool] $Saved,
+        [string] $SavedPlatform = "Windows"
+    )
+    Assert-UEEnvironment
+    if ($Saved) {
+        return Resolve-Path "$CurrentProjectDirectory/Saved/Config/$SavedPlatform/$ConfigName.ini"
+    }
+    else {
+        return Resolve-Path "$CurrentProjectDirectory/Config/Default$ConfigName.ini"
+    }
+}
+
+function Get-UEConfig {
+    param(
+        [parameter(Mandatory = $true)] [string] $ConfigName,
+        [switch] $Saved,
+        [string] $SavedPlatform = "Windows"
+    )
+    $IniPath = Get-UEConfigPath -ConfigName $ConfigName -Saved $Saved -SavedPlatform $SavedPlatform
+    Get-IniContent -Path $IniPath
+}
+
+function Set-UEConfig {
+    [cmdletbinding()]
+    param (
+        [parameter(ValueFromPipeline)] [hashtable] $Data,
+        [parameter(Mandatory = $true)] [string] $ConfigName,
+        [switch] $Saved,
+        [string] $SavedPlatform = "Windows"
+    )
+    $IniPath = Get-UEConfigPath -ConfigName $ConfigName -Saved $Saved -SavedPlatform $SavedPlatform
+    $Data | Set-IniContent -Path $IniPath
+}
+
+function Set-UEConfigValue {
+    [cmdletbinding()]
+    param (
+        [parameter(ValueFromPipeline)] [string] $Value,
+        [parameter(Mandatory = $true)] [string] $ConfigName,
+        [parameter(Mandatory = $true)] [string] $Key,
+        [parameter(Mandatory = $true)] [string] $Section,
+        [switch] $Saved,
+        [string] $SavedPlatform = "Windows"
+    )
+
+    $IniPath = Get-UEConfigPath -ConfigName $ConfigName -Saved $Saved -SavedPlatform $SavedPlatform
+    $Value | Set-IniValue -Path $IniPath -Key $Key -Section $Section
+}
+
+function Get-UEProjectVersion {
+    param([parameter()] [string] $FallbackVersion = "0.1.0")
+
+    $GeneralProjectSettings = (Get-UEConfig "Game")."/Script/EngineSettings.GeneralProjectSettings"
+    if ($GeneralProjectSettings.ContainsKey("ProjectVersion")) {
+        return $GeneralProjectSettings.ProjectVersion
+    }
+    else {
+        return "$FallbackVersion"
+    }
+}
+
+function Set-UEProjectVersion {
+    param([parameter(Mandatory = $true)] [string] $Version)
+    $Version | Set-UEConfigValue -ConfigName "Game" -Key "ProjectVersion" -Section "/Script/EngineSettings.GeneralProjectSettings"
+}
+
+Export-ModuleMember -Function Open-UEProject, Get-UEProgramPath, Start-UE, Start-UETests, Start-UECommandlet, Get-UEConfig, Set-UEConfig, Set-UEConfigValue, Get-UEProjectVersion, Set-UEProjectVersion
