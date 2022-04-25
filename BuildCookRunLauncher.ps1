90652f7e41359469fb49ea85c87db9ad97a4013c
Jonas Reich <mail@jonasreich.de>
2022-04-26 01:45:50 +0200

    Adjusted BuildCookRunLauncher for UE5 and added combo box for build config

--- BuildCookRunLauncher.ps1
+++ BuildCookRunLauncher.ps1
@@ -7,7 +7,7 @@ powershell.exe -ExecutionPolicy RemoteSigned -File .../OpenUnrealAutomationTools
 
 param(
     [String]
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     $ProjectPath
 )
 
@@ -30,56 +30,99 @@ $UProject = Open-UEProject $ProjectPath
 $GameIni = Get-UEConfig Game
 $UECmdPath = Get-UEProgramPath EditorCmd
 
-$MapIniSectionNames = $GameIni["MapsToCook"]["ConfigSections"]
+if ($GameIni.ContainsKey("MapsToCook")) {
+    $MapIniSectionNames = $GameIni["MapsToCook"]["ConfigSections"]
+    Write-Verbose "Detected the following map ini section names: $MapIniSectionNames"
+}
+else {
+    Write-Warning "Expected a config section called [MapsToCook] in DefaultGame.ini to configure which map sections to use for map cook lists."
+    $MapIniSectionNames = @("")
+}
+
 $MapIniSection = $MapIniSectionNames[0]
+
+$Configuration = "Development"
+$Configurations = @($Configuration, "DebugGame", "Shipping", "Test")
 
 ## WINFORMS GUI FOR LAUNCHER
 
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
 [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
 $WinForm = New-Object System.Windows.Forms.Form
-$WinForm.Backcolor="white"
-$WinForm.Size = New-Object System.Drawing.Size(350,130)
-$WinForm.FormBorderStyle = 'Fixed3D'
-$WinForm.MaximizeBox = $false
+$WinForm.Backcolor = "lightgray"
 $WinForm.Text = "Build Cook Run Launcher"
 # Use Unreal Engine icon for our launcher
-$WinForm.Icon=[System.Drawing.Icon]::ExtractAssociatedIcon($UECmdPath)
-
-$PromptLabel = New-Object System.Windows.Forms.Label
-$PromptLabel.Location = New-Object System.Drawing.Size(5, 5)
-$PromptLabel.Size = New-Object System.Drawing.Size(200,20)
-$PromptLabel.Text = "MapsToCook config section"
-$WinForm.Controls.Add($PromptLabel)
-
-$IniSectionCombobox = New-Object System.Windows.Forms.Combobox
-$IniSectionCombobox.Location = New-Object System.Drawing.Size(5, 30)
-$IniSectionCombobox.Size = New-Object System.Drawing.Size(200,20)
-$IniSectionCombobox.Height = 70
-$IniSectionCombobox.DropDownStyle = [System.Windows.Forms.ComboBoxStyle]::DropDownList;
-$WinForm.Controls.Add($IniSectionCombobox)
+$WinForm.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($UECmdPath)
 $WinForm.Topmost = $True
-foreach ($IniSectionName in $MapIniSectionNames) {
-    $IniSectionCombobox.Items.Add($IniSectionName) | Out-Null
+
+$VerticalPadding = 15
+$HorizontalPadding = 10
+$AccumulatedHeight = $VerticalPadding
+
+function AddCombobox {
+    param(
+        $LabelText,
+        $Items
+    )
+
+    $Label = New-Object System.Windows.Forms.Label
+    $Label.Location = New-Object System.Drawing.Size($HorizontalPadding, $script:AccumulatedHeight)
+    $script:AccumulatedHeight += 20
+    $Label.Size = New-Object System.Drawing.Size(200, 20)
+    $Label.Text = $LabelText
+    $WinForm.Controls.Add($Label)
+
+    $Combobox = New-Object System.Windows.Forms.Combobox
+    $Combobox.Location = New-Object System.Drawing.Size($HorizontalPadding, $script:AccumulatedHeight)
+    $Combobox.Size = New-Object System.Drawing.Size(200, 20)
+    $Combobox.Height = 70
+    $script:AccumulatedHeight += 20 + $VerticalPadding
+    $Combobox.DropDownStyle = [System.Windows.Forms.ComboBoxStyle]::DropDownList;
+    foreach ($Item in $Items) {
+        $Combobox.Items.Add($Item) | Out-Null
+    }
+    $WinForm.Controls.Add($Combobox)
+    $Combobox.SelectedIndex = 0
+
+    return $Combobox
 }
-$IniSectionCombobox.SelectedIndex = 0
+
+# Maps to cook combobox
+$IniSectionCombobox = AddCombobox -LabelText "MapsToCook config section" -Items ($MapIniSectionNames)
 $IniSectionCombobox.Add_SelectedIndexChanged({
     $script:MapIniSection = $IniSectionCombobox.Text
 })
 
+# Build configuration combobox
+$ConfigurationCombobox = AddCombobox -LabelText "Build Config (Server + Client)" -Items ($Configurations)
+$ConfigurationCombobox.Add_SelectedIndexChanged({
+    $script:Configuration = $ConfigurationCombobox.Text
+})
+
+# Additional padding before launch button
+$AccumulatedHeight += $VerticalPadding
+
+# Launch button
 $LaunchClicked = $False
 $LaunchButton = New-Object System.Windows.Forms.Button
-$LaunchButton.Location = New-Object System.Drawing.Size(5,55)
-$LaunchButton.Size = New-Object System.Drawing.Size(75,20)
+$LaunchButton.Location = New-Object System.Drawing.Size($HorizontalPadding, $AccumulatedHeight)
+$AccumulatedHeight += 20 + $VerticalPadding
+$LaunchButton.Size = New-Object System.Drawing.Size(75, 20)
 $LaunchButton.Text = "Launch"
 $LaunchButton.Name = "Launch"
 $LaunchButton.Add_Click({ 
-    $script:LaunchClicked = $True
-    $WinForm.Close() 
-})
+        $script:LaunchClicked = $True
+        $WinForm.Close() 
+    })
 $WinForm.Controls.Add($LaunchButton)
 
-# Shows the winform and pauses
+# Adjust size based on content (no scrolling / fixed size)
+$TitleBarHeight = 40
+$WinForm.Size = New-Object System.Drawing.Size(350, ($AccumulatedHeight + $TitleBarHeight))
+$WinForm.FormBorderStyle = 'Fixed3D'
+$WinForm.MaximizeBox = $false
+
+# Shows the winform and pauses until closed
 [void] $WinForm.ShowDialog()
 
 ## REACT TO GUI INPUTS / LAUNCH BuildCookRun
@@ -87,8 +130,16 @@ $WinForm.Controls.Add($LaunchButton)
 if ($LaunchClicked) {
     # TODO Add before -run parameter (?)
     # -cmdline=" -Messaging" -device=WindowsNoEditor@DEUMUCCLW032 -addcmdline="-SessionId=E6ACA1E245978B313340D4A46A0995FD -SessionOwner='jreich' -SessionName='BuildCookLaunch_TQ2_Development'  "
-        
-    Start-UE UAT -ScriptsForProject="$ProjectPath" BuildCookRun -project="$ProjectPath" -noP4 -clientconfig=Development -serverconfig=Development -nocompile -nocompileeditor -installed -ue4exe="$UECmdPath" -utf8output -platform=Win64 -targetplatform=Win64 -build -cook -additionalcookeroptions="-MAPINISECTION=$MapIniSection" -iterativecooking -SkipCookingEditorContent -compressed -iterativedeploy -stage -deploy -run
-} else {
+    $ExecutableArg = if ($script:UEMajorVersion -gt 4) { "-unrealexe=$UECmdPath" } else { "-ue4exe=$UECmdPath" }
+    $ConfigurationArgs = @("-clientconfig=$Configuration", "-serverconfig=$Configuration")
+
+    # TODO: Is this the right condition / what is the flag used for? Is this dependent on the engine version or the desired build output?
+    $InstalledArg = if ($UEIsInstalledBuild) { "-installed" } else { @() }
+
+    $PlatformArgs = @("-platform=Win64", "-targetplatform=Win64")
+    
+    Start-UE UAT -ScriptsForProject="$ProjectPath" BuildCookRun -project="$ProjectPath" $ExecutableArg -noP4 $ConfigurationArgs -nocompile -nocompileeditor $InstalledArg -utf8output $PlatformArgs -build -cook "-additionalcookeroptions='-MAPINISECTION=$MapIniSection'" -iterativecooking -SkipCookingEditorContent -compressed -iterativedeploy -stage -deploy -run
+}
+else {
     Write-Warning "Operation was canceled by user"
 }
