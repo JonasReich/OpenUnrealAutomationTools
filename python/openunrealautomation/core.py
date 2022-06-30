f953fc9fd3707a98225fc6d98496d890dba0e963
Jonas Reich <mail@jonasreich.de>
2022-06-30 16:10:36 +0200

    Moved utility functions from core to util

--- python/openunrealautomation/core.py
+++ python/openunrealautomation/core.py
@@ -1,7 +1,8 @@
+"""
+Small core types (esp. Enums)
+"""
+
 from enum import Enum
-import os
-import pathlib
-from typing import Generator
 
 
 class OUAException(Exception):
@@ -39,29 +40,3 @@ class UnrealBuildTarget(Enum):
 
     def __str__(self) -> str:
         return self.value[1]
-
-
-def walklevel(top, topdown=True, onerror=None, followlinks=False, level=1):
-    """
-    Copy of os.walk() with additional level parameter.
-    @param level    How many sub-directories to traverse
-    """
-    # From https://stackoverflow.com/a/234329
-    top = top.rstrip(os.path.sep)
-    assert os.path.isdir(top)
-    num_sep = top.count(os.path.sep)
-    for root, dirs, files in os.walk(top, topdown=topdown, onerror=onerror, followlinks=followlinks):
-        yield root, dirs, files
-        num_sep_this = root.count(os.path.sep)
-        if num_sep + level <= num_sep_this:
-            del dirs[:]
-
-
-def walkparents(dir: str) -> Generator[str, None, None]:
-    """Go through all parent directories of the given dir."""
-    path = pathlib.Path(dir)
-    while True:
-        yield str(path)
-        if (path.parent == path):
-            break
-        path = path.parent
