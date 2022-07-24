28f5d17d322b6b406cd01033c50fe19e8f3a25fc
Jonas Reich <mail@jonasreich.de>
2022-07-24 14:42:01 +0200

    Added FASTBuild  to setuptools

--- python/openunrealautomation/setuptools.py
+++ python/openunrealautomation/setuptools.py
@@ -7,6 +7,9 @@ import re
 import subprocess
 import time
 import urllib
+import urllib.error
+
+import requests
 from pathlib import Path
 
 import vswhere
@@ -47,7 +50,7 @@ class VisualStudioCode:
         If appending, existing keys can only be rewritten, not deleted.
         """
         config_data = new_data if replace else {
-            **self.get_config(), **new_data}
+            **self.get_settings(), **new_data}
         with open(self._get_settings_path(), "w") as config_file:
             json.dump(config_data, config_file, indent=4)
 
@@ -56,7 +59,7 @@ class VisualStudioCode:
         Install an extension by its identifier.
         (e.g. "ms-vscode.cpptools" for Microsoft C/C++ tools)
         """
-        return subprocess.run([self.vscode.exe_path, "--install-extension", identifier], check=True)
+        return subprocess.run([self.exe_path, "--install-extension", identifier], check=True)
 
     def _get_settings_path(self) -> str:
         return os.path.join(
@@ -100,19 +103,19 @@ class VisualStudio:
             raise VisualStudioNotFoundException(
                 "No applicable installation of VisualStudio found")
 
-        self.version = latest_applicable_version["productDisplayVersion"]
-        self.product_line_version = latest_applicable_version["productLineVersion"]
+        self.version = latest_applicable_version["catalog"]["productDisplayVersion"]
+        self.product_line_version = latest_applicable_version["catalog"]["productLineVersion"]
 
         version_match = re.search(
-            "(?P<major>\\d+).(?P<minor>\\d+).(?P<patch>\\d+)", )
-        self.major_version = version_match.group("major")
-        self.minor_version = version_match.group("minor")
-        self.patch_version = version_match.group("patch")
+            "(?P<major>\\d+).(?P<minor>\\d+).(?P<patch>\\d+)", self.version)
+        self.major_version = int(version_match.group("major"))
+        self.minor_version = int(version_match.group("minor"))
+        self.patch_version = int(version_match.group("patch"))
 
         self.installation_path = latest_applicable_version["installationPath"]
 
-        print("Found Visual Studio ", self.product_line_version,
-              "Installation (", self.version, "):", self.installation_path)
+        print(
+            f"Found Visual Studio {self.product_line_version} Installation ({self.version}): {self.installation_path}")
 
     def install_extension(self, vsix_path: str) -> int:
         vsix_installer = os.path.join(
@@ -125,9 +128,10 @@ class VisualStudio:
     def download_and_install_extension(self, name: str, download_url: str, retries: int = 3) -> int:
         downloads_dir = os.path.join(
             str(Path.home()), "Downloads/VSExtensions", self.product_line_version)
+        os.makedirs(downloads_dir, exist_ok=True)
 
         # to get content after redirection
-        download_src = urllib.request.get(
+        download_src = requests.get(
             download_url, allow_redirects=True).url
         download_target = os.path.join(downloads_dir, name)
 
@@ -142,9 +146,10 @@ class VisualStudio:
                 try:
                     download_success = True
                     urllib.request.urlretrieve(download_src, download_target)
-                except urllib.HTTPError as http_error:
+                except urllib.error.HTTPError as http_error:
                     download_success = False
-                    if http_error.errno == 429:
+                    print("Received HTTPError: ", str(http_error))
+                    if http_error.code == 429:
                         if num_retries > retries:
                             raise OUAException(
                                 f"Ran out of retries to download {download_src}")
@@ -155,12 +160,11 @@ class VisualStudio:
                         raise http_error
                 if download_success:
                     break
+
         return self.install_extension(download_target)
 
     def install_unrealvs(self):
         """Install the unrealvs extension shipped with UE"""
-
-        self.environment.engine_root
 
         version_folder = f"VS{self.product_line_version}"
         vsix_path = os.path.join(
@@ -170,3 +174,59 @@ class VisualStudio:
             "UnrealVS.vsix")
 
         self.install_extension(vsix_path=vsix_path)
+
+
+class FASTBuildCacheMode(Enum):
+    READ = 0, "r"
+    WRITE = 1, "w"
+    READ_WRITE = 2, "rw"
+
+    def __str__(self) -> str:
+        return self.value[1]
+
+
+class FASTBuild:
+    environment: UnrealEnvironment = None
+
+    def __init__(self, environment: UnrealEnvironment) -> None:
+        self.environment = environment
+
+    def setup(self, brokerage_path: str = None, cache_path: str = None, cache_mode: FASTBuildCacheMode = None) -> None:
+        """Perform complete setup (brokerage path, startup registration, """
+        self.set_environment_variables(brokerage_path=brokerage_path,
+                                       cache_path=cache_path,
+                                       cache_mode=cache_mode)
+        self.add_buildworker_startup()
+        self.start_buildworker()
+
+    def add_buildworker_startup(self) -> None:
+        """Add the Buildworker to startup/autostart"""
+        add_startup("FBuildWorker", self.get_buildworker_exe(),
+                    ["-nosubprocess"])
+
+    def set_environment_variables(self, brokerage_path: str = None, cache_path: str = None, cache_mode: FASTBuildCacheMode = None) -> None:
+        """
+        Permanently store the brokerage path, cache path and cache setting to the system environment.
+        Alternatively you can also set those settings in the command line environment only.
+        """
+        if brokerage_path is not None:
+            set_system_env_var("FASTBUILD_BROKERAGE_PATH", brokerage_path)
+        if cache_path is not None:
+            set_system_env_var("FASTBUILD_CACHE_PATH", cache_path)
+        if cache_mode is not None:
+            set_system_env_var("FASTBUILD_CACHE_MODE", str(cache_mode))
+
+    def start_buildworker(self) -> None:
+        # Do not use call -> We do not want to wait for the process to exit
+        subprocess.Popen([self.get_buildworker_exe(), "-nosubprocess"])
+
+    def get_buildworker_exe(self) -> str:
+        """Returns the path to the buildworker executable"""
+        return os.path.join(self._bin_path(), "FBuildWorker.exe")
+
+    def _bin_path(self) -> str:
+        return os.path.join(self.environment.engine_root, "Engine/Extras/ThirdPartyNotUE/FASTBuild/Win64")
+
+
+if __name__ == "__main__":
+    VisualStudio("[16.0,18.0)")
