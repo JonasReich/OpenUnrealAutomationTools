55a0834698bf135be4e14d712fd95c796f83c1df
Jonas Reich <mail@jonasreich.de>
2026-01-03 04:20:43 +0100

    Deleted all PowerShell scripts

--- Samples/SampleScript.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set "LogDir=%~dp0../Saved/Logs"
-PowerShell "if (-not (Test-Path '%LogDir%')) { New-Item -ItemType 'Directory' -Path '%LogDir%' }"
-PowerShell "%~dp0SampleScript.ps1 -RootFolder %* *>&1 | Tee-Object -FilePath %LogDir%/Log.log"
